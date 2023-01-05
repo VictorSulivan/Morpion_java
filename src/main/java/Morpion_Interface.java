a2932250bf43bdca62e21c8e5cd1938a5e6e0ba4
Floria <florian.richet2004@gmail.com>
2023-01-05 11:59:25 +0100

    v1.1/Francais

--- src/main/java/Morpion_Interface.java
+++ src/main/java/Morpion_Interface.java
@@ -16,63 +16,63 @@ public class Morpion_Interface {
 
         final boolean[] joueur1 = {true};
 
-        JLabel[] labels = new JLabel[9];
+        JLabel[] cases = new JLabel[9];
         for (int i = 0 ; i < 9; i++) {
 
-            labels[i] = new JLabel(""+i);
-            labels[i].setBounds((i % 3) * 100, (i / 3) * 100, 100, 100);
-            labels[i].setHorizontalAlignment(JLabel.CENTER);
-            labels[i].setVerticalAlignment(JLabel.CENTER);
-            frame.add(labels[i]);
+            cases[i] = new JLabel(""+i);
+            cases[i].setBounds((i % 3) * 100, (i / 3) * 100, 100, 100);
+            cases[i].setHorizontalAlignment(JLabel.CENTER);
+            cases[i].setVerticalAlignment(JLabel.CENTER);
+            frame.add(cases[i]);
             final int index = i;
-            labels[i].addMouseListener(new MouseAdapter() {
+            cases[i].addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if (joueur1[0] == true) {
-                        labels[index].setText("X");
+                        cases[index].setText("X");
                         joueur1[0] = false;
                     } else {
-                        labels[index].setText("O");
+                        cases[index].setText("O");
                         joueur1[0] = true;
                     }
 
                     //CONDITION DE VICTOIRE À L'HORIZONTALE
-                    if (labels[0].getText().equals(labels[1].getText()) && labels[1].getText().equals(labels[2].getText()) && !labels[0].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[0].getText() + " a gagné !");
+                    if (cases[0].getText().equals(cases[1].getText()) && cases[1].getText().equals(cases[2].getText()) && !cases[0].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[0].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
-                    if (labels[3].getText().equals(labels[4].getText()) && labels[4].getText().equals(labels[5].getText()) && !labels[3].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[3].getText() + " a gagné !");
+                    if (cases[3].getText().equals(cases[4].getText()) && cases[4].getText().equals(cases[5].getText()) && !cases[3].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[3].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
-                    if (labels[6].getText().equals(labels[7].getText()) && labels[7].getText().equals(labels[8].getText()) && !labels[6].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[6].getText() + " a gagné !");
+                    if (cases[6].getText().equals(cases[7].getText()) && cases[7].getText().equals(cases[8].getText()) && !cases[6].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[6].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
                     //CONDITION DE VICTOIRE À L'HORIZONTALE
 
                     //CONDITION DE VICTOIRE À LA VERTICALE
-                    if (labels[0].getText().equals(labels[3].getText()) && labels[3].getText().equals(labels[6].getText()) && !labels[0].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[0].getText() + " a gagné !");
+                    if (cases[0].getText().equals(cases[3].getText()) && cases[3].getText().equals(cases[6].getText()) && !cases[0].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[0].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
-                    if (labels[1].getText().equals(labels[4].getText()) && labels[4].getText().equals(labels[7].getText()) && !labels[1].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[1].getText() + " a gagné !");
+                    if (cases[1].getText().equals(cases[4].getText()) && cases[4].getText().equals(cases[7].getText()) && !cases[1].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[1].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
-                    if (labels[2].getText().equals(labels[5].getText()) && labels[5].getText().equals(labels[8].getText()) && !labels[2].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[2].getText() + " a gagné !");
+                    if (cases[2].getText().equals(cases[5].getText()) && cases[5].getText().equals(cases[8].getText()) && !cases[2].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[2].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
                     //CONDITION DE VICTOIRE À LA VERTICALE
 
                     //CONDITION DE VICTOIRE EN DIAGONALE
-                    if (labels[0].getText().equals(labels[4].getText()) && labels[4].getText().equals(labels[8].getText()) && !labels[0].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[0].getText() + " a gagné !");
+                    if (cases[0].getText().equals(cases[4].getText()) && cases[4].getText().equals(cases[8].getText()) && !cases[0].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[0].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
-                    if (labels[2].getText().equals(labels[4].getText()) && labels[4].getText().equals(labels[6].getText()) && !labels[2].getText().equals("")) {
-                        System.out.println("Le joueur " + labels[2].getText() + " a gagné !");
+                    if (cases[2].getText().equals(cases[4].getText()) && cases[4].getText().equals(cases[6].getText()) && !cases[2].getText().equals("")) {
+                        System.out.println("Le joueur " + cases[2].getText() + " a gagné !");
                         frame.setVisible(false);
                     }
                     //CONDITION DE VICTOIRE EN DIAGONALE
